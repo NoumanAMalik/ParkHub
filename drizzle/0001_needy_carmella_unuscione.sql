47b1c727d177e3ccd875bcf10993649f24e48c71
Nouman <noumancodes@gmail.com>
2023-10-04 14:08:25 -0400

    removed snapshots

--- drizzle/0001_needy_carmella_unuscione.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `countries` DROP CONSTRAINT `name_idx`;
--- drizzle/0002_mushy_steel_serpent.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `cities` DROP FOREIGN KEY `cities_country_id_countries_id_fk`;
---> statement-breakpoint
-ALTER TABLE `cities` DROP COLUMN `country_id`;--> statement-breakpoint
-ALTER TABLE `countries` ADD CONSTRAINT `name_idx` UNIQUE(`name`);
