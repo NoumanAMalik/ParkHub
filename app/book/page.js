dbdae71a2295ca28c883289383208953fbcb444b
Nouman <noumancodes@gmail.com>
2023-11-27 19:01:33 -0500

    modal

--- app/book/page.js
+++ app/book/page.js
@@ -1,13 +1,43 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import Input from "@/components/input";
 
 const Book = () => {
     const searchParams = useSearchParams();
     const [parkingData, setParkingData] = useState([]);
+    const [selectedData, setSelectedData] = useState({});
+    const [submitData, setSubmitData] = useState(false);
+    const [formData, setFormData] = useState({
+        hours: "",
+    });
+
+    const formUpdate = (event) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [event.target.id]: event.target.value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        setSubmitData(true);
+    };
 
     const randNum = () => {
         return Math.floor(Math.random() * 10000 + 1);
+    };
+
+    const handleRowClick = (data) => {
+        setSelectedData(data);
+        const modal = document.querySelector("#bookingModal");
+        modal.classList.add("modal-open");
+    };
+
+    const closeModal = () => {
+        const modal = document.querySelector("#bookingModal");
+        modal.classList.remove("modal-open");
     };
 
     useEffect(() => {
@@ -23,6 +53,10 @@ const Book = () => {
 
         getParkingLotData();
     }, []);
+
+    useEffect(() => {
+        console.log(formData);
+    }, [submitData]);
 
     return (
         <div className="flex flex-col items-center justify-center">
@@ -52,7 +86,11 @@ const Book = () => {
                         <tbody>
                             {parkingData.map((data) => {
                                 return (
-                                    <tr key={randNum()}>
+                                    <tr
+                                        key={randNum()}
+                                        onClick={() => handleRowClick(data)}
+                                        className="hover"
+                                    >
                                         <th key={randNum()}>{data.id}</th>
                                         <td key={randNum()}>{data.name}</td>
                                         <td key={randNum()}>{data.location}</td>
@@ -65,6 +103,43 @@ const Book = () => {
                             })}
                         </tbody>
                     </table>
+                </div>
+            </div>
+
+            <div className="modal" id="bookingModal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg underline">
+                        Book Parking
+                    </h3>
+                    <p>Name: {selectedData?.name || "No Data"}</p>
+                    <p>Location: {selectedData?.location || "No Data"}</p>
+                    <p>
+                        Spaces Available:{" "}
+                        {selectedData?.spacesAvailable || "No Data"}
+                    </p>
+                    <p>Price: {selectedData?.price || "No Data"}</p>
+                    <Input
+                        uniqueId={1}
+                        id="hours"
+                        label="Enter the number of hours you would like to book for?"
+                        placeholder="Type here"
+                        changeHandler={formUpdate}
+                    />
+                    <button
+                        className="btn btn-wide btn-outline btn-secondary mt-6"
+                        onClick={handleSubmit}
+                    >
+                        Submit
+                    </button>
+                    <div className="modal-action">
+                        <a
+                            href="#"
+                            className="btn"
+                            onClick={() => closeModal()}
+                        >
+                            Close
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
